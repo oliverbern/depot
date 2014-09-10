40c2e987220a7ea7a313ed1cb981b3bb05979c5f
Oliver Jones <oapjones@gmail.com>
2014-09-10 04:15:32 +0200

    checkout cart installed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :orders
+
   resources :line_items
 
   resources :carts
@@ -8,6 +10,10 @@ Rails.application.routes.draw do
   resources :products
 
   root 'store#index'
+
+  resources :products do
+      get :who_bought, on: :member
+    end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
