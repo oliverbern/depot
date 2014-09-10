c802bd2ccee94f1d704e8fcfc7bbbe55630d4346
Oliver Jones <oapjones@gmail.com>
2014-09-10 02:55:20 +0200

    added cart, ajax and clickable images

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,6 @@
 class CartsController < ApplicationController
   before_action :set_cart, only: [:show, :edit, :update, :destroy]
+  rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
 
   # GET /carts
   # GET /carts.json
@@ -53,12 +54,20 @@ class CartsController < ApplicationController
 
   # DELETE /carts/1
   # DELETE /carts/1.json
-  def destroy
-    @cart.destroy
+def destroy
+    @cart.destroy if @cart.id == session[:cart_id]
+    session[:cart_id] = nil
     respond_to do |format|
-      format.html { redirect_to carts_url, notice: 'Cart was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+        format.html { redirect_to store_index_path}
+        format.json {head :no_content}
+     end
+  end
+
+
+  #rescure from ActiveError
+  def invalid_cart
+    logger.error "Attempt to access invalid cart #{params[:id]}"
+    redirect_to store_index_path, notice: 'Invalid Cart'
   end
 
   private
